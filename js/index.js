607b533b9ae8d531ca8af0b00942577dd712f2aa
Salman Basha <salmanbbasha@gmail.com>
2022-09-19 17:06:52 +0530

    Step 3 updates

--- js/index.js
+++ js/index.js
@@ -16,14 +16,35 @@ rulesButton.addEventListener('click', popup);
 const gameCircle = document.querySelectorAll('.choose');
 const gamePlay = document.querySelector('.game-play');
 const playerPick = document.querySelector('.player-pick');
+const housePick = document.querySelector('.house-pick');
 
 let arr = Array.from(gameCircle);
+let tempArr;
 let playerPicked;
+let housePicked;
+
+function getRandomItem(list) {
+  let randomIndex = Math.floor(Math.random() * list.length);
+
+  let item = list[randomIndex];
+
+  return item;
+}
 
 arr.forEach(div => {
   div.addEventListener('click', function () {
     gamePlay.classList.toggle('disable');
     playerPicked = div.outerHTML;
     playerPick.innerHTML = playerPicked;
+    /* Non-Choosed items */
+    tempArr = arr.filter(a => {
+      return div.dataset.id !== a.dataset.id;
+    });
+    /* Getting random item from non-choosed item */
+    housePicked = getRandomItem(tempArr);
+    setTimeout(function () {
+      housePick.innerHTML = housePicked.outerHTML;
+      console.log(housePicked.dataset.id);
+    }, 2000);
   });
 });
