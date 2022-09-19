22aea44644843e075d57579c17d330a7311062f9
Salman Basha <salmanbbasha@gmail.com>
2022-09-19 16:25:47 +0530

    Step 2 updates

--- js/index.js
+++ js/index.js
@@ -1,14 +1,29 @@
+/* Rules */
 const rulesButton = document.querySelector('.btn');
 const rulesContent = document.querySelector('.rules-content');
 const overlay = document.querySelector('.overlay');
-
 const closeIcon = document.querySelector('.close-icon');
 
 function popup() {
   rulesContent.classList.toggle('popup-active');
   overlay.classList.toggle('overlay-active');
 }
-
 closeIcon.addEventListener('click', popup);
 
 rulesButton.addEventListener('click', popup);
+
+/* Game */
+const gameCircle = document.querySelectorAll('.choose');
+const gamePlay = document.querySelector('.game-play');
+const playerPick = document.querySelector('.player-pick');
+
+let arr = Array.from(gameCircle);
+let playerPicked;
+
+arr.forEach(div => {
+  div.addEventListener('click', function () {
+    gamePlay.classList.toggle('disable');
+    playerPicked = div.outerHTML;
+    playerPick.innerHTML = playerPicked;
+  });
+});
