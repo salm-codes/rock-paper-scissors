8749ea7685eee303009adf1514b10dddc08217fe
Salman Basha <salmanbbasha@gmail.com>
2022-09-20 13:08:02 +0530

    Added sound effects

--- js/index.js
+++ js/index.js
@@ -15,6 +15,7 @@ rulesButton.addEventListener('click', popup);
 /* Game */
 const gameCircle = document.querySelectorAll('.choose');
 const gamePlay = document.querySelector('.game-play');
+const gamePlay2 = document.querySelector('.game-play-s2');
 const playerPick = document.querySelector('.player-pick');
 const housePick = document.querySelector('.house-pick');
 const scorePoints = document.querySelector('.score-points');
@@ -22,6 +23,12 @@ const scorePoints = document.querySelector('.score-points');
 let arr = Array.from(gameCircle);
 let tempArr;
 let score = 0;
+
+/* Sounds */
+let playwin = new Audio('../sounds/mixkit-winning-notification-2018.wav');
+let playLose = new Audio('../sounds/mixkit-losing-bleeps-2026.wav');
+let pick = new Audio('../sounds/mixkit-game-ball-tap-2073.wav');
+let bubble = new Audio('../sounds/mixkit-water-bubble-1317.wav');
 
 function getRandomItem(list) {
   let randomIndex = Math.floor(Math.random() * list.length);
@@ -36,16 +43,21 @@ function update(p, h, s) {
     score = score + 1;
     s.textContent = `${score}`;
     document.querySelector('.result2').classList.add('disable');
+    playwin.play();
   } else {
     score = score - 1;
     s.textContent = `${score}`;
     document.querySelector('.result1').classList.add('disable');
+    playLose.play();
   }
 }
 
 arr.forEach(div => {
   div.addEventListener('click', function () {
+    pick.play();
     gamePlay.classList.add('disable');
+    gamePlay2.classList.toggle('disable');
+
     let playerPicked = div;
     playerPick.innerHTML = playerPicked.outerHTML;
     /* Non-Choosed items */
@@ -59,17 +71,19 @@ arr.forEach(div => {
 
     setTimeout(function () {
       housePick.innerHTML = housePicked.outerHTML;
+      bubble.play();
     }, 1000);
 
     setTimeout(() => {
       document.querySelector('.play-again').classList.toggle('disable');
       update(pid, hid, scorePoints);
-    }, 1500);
+    }, 2000);
   });
 });
 
 document.querySelector('.btn-play').addEventListener('click', function () {
   gamePlay.classList.remove('disable');
+  gamePlay2.classList.toggle('disable');
   tempArr = [];
   housePick.innerHTML = '<div class="temp-circle"></div>';
   document.querySelector('.play-again').classList.toggle('disable');
